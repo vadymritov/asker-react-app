2a988fdd8bacf3f37c18de66d24a76afcf781b3f
rajesh-coretus <rajesh.nakum@coretus.com>
2021-12-22 14:48:26 +0530

    add fevicon icon and siteurl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Asker || Home page</title>
+    <title>Asker</title>
+    <link rel="icon" type="image/png" href="{{asset('fevicon.png')}}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
